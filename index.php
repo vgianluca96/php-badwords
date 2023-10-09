d0b6ea515e07c107206389534cb217c224880e6b
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-09 15:10:09 +0200

    inserito il form

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+
+    <form action="script.php" method="">
+        <input type="text" placeholder="Inserire un frase" size="50"> <br>
+        <input type="text" placeholder="Inserire la parola da censurare" size="50"> <br>
+        <button>Invia</button>
+    </form>
+
+</body>
+
+</html>
