20a04236ea4bd813c21ac6ce0b2dd6f443b02333
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-09 15:27:26 +0200

    task completato
    aggiunte istruzioni in script.php per censurare la parola

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
 
     <form action="script.php" method="post">
-        <input type="text" name="paragraph" placeholder="Inserire un frase" size="50"> <br>
+        <input type="text" name="paragraph" placeholder="Inserire una frase" size="50"> <br>
         <input type="text" name="word" placeholder="Inserire la parola da censurare" size="50"> <br>
         <button>Invia</button>
     </form>
--- script.php
+++ script.php
@@ -1,3 +1,13 @@
+<?php
+
+$paragraph = $_POST["paragraph"];
+$wordToReplace = $_POST["word"];
+$wordToUse = '***';
+$paragaphCensored = str_replace($wordToReplace, $wordToUse, $paragraph);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,11 +22,15 @@
     <div>
         <h2>Paragrafo:</h2>
         <p>
-            <?php echo $_POST["paragraph"]; ?>
+            <?php echo $paragraph; ?>
         </p>
         <h3>Parola da censurare:</h3>
         <p>
-            <?php echo $_POST["word"]; ?>
+            <?php echo $wordToReplace; ?>
+        </p>
+        <h3>Paragrafo con parola censurata:</h3>
+        <p>
+            <?php echo $paragaphCensored; ?>
         </p>
     </div>
 
